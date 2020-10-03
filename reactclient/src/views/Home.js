7c0f1e9c6dbc533e8b80ff505e264004171e1fc1
Rishi N Desai <rishindesai@yahoo.com>
2020-10-03 05:59:26 +0530

    landing page UI

--- reactclient/src/views/Home.js
+++ reactclient/src/views/Home.js
@@ -1,28 +1,52 @@
-import React, { memo, useState, useEffect } from 'react';
-import { useSelector, shallowEqual, useDispatch } from 'react-redux';
-import Header from '../components/Header';
-import { Link } from 'react-router-dom';
+import React, { memo } from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import Avatar from '@material-ui/core/Avatar';
 import './styles/home.css'
 
 const Home = (props) => {
-    const isLoggedIn = useSelector(state => state.AuthReducer.isLoggedIn, shallowEqual)
-    const dispatch = useDispatch()
+    const { isLoggedIn, role, profile } = useSelector(state => ({ 
+        isLoggedIn: state.AuthReducer.isLoggedIn,
+        role: state.AuthReducer.role,
+        profile: state.AuthReducer.profile
+    }))
+    const history = useHistory()
+
+    const navigationHandler = () => {
+        if(isLoggedIn){
+            history.push("/dashboard")
+        }
+        else{
+            history.push("/login")
+        }
+    }
 
     return (
         <div id="background">
             <div id="main-content">
-                <h1 style={{ color: 'white' }}>Apply for a job</h1>
-                <h1 style={{ color: 'white' }}>Pre-interview on same portal</h1>
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent:'center', alignItems:'center' }}>
+                    <h1 className="main-content-text">Apply for a job</h1>
+                    <CheckCircleIcon style={{ color: 'green', fontSize: 40, marginLeft: 10 }} />
+                </div>
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent:'center', alignItems:'center' }}>
+                    <h1 className="main-content-text">Pre-Interview on same portal</h1>
+                    <CheckCircleIcon style={{ color: 'green', fontSize: 40, marginLeft: 10 }} />
+                </div>
+                <Button variant="contained" style={{ backgroundColor: '#40E0D0', margin: 25, fontFamily: "MontserratRegular", fontWeight: 600 }} onClick={() => {
+                    history.push("/jobs")
+                }}>
+                    Browse Jobs
+                </Button>
             </div>
-            {console.log("render home")}
-            <div>
+            <div onClick={navigationHandler} style={{ cursor: 'pointer' }}>
                 {isLoggedIn ? 
-                    <Link to="/dashboard">Dashboard</Link>
-                : 
-                    <Link to="/login">login</Link>
+                    <Avatar src={role === "employer" ? profile.logo : profile.profile_pic} className="profileButton" />    
+                :
+                    <p className="loginButton">Login</p>
                 }
             </div>
-            <Link to="/jobs">jobs</Link>
         </div>
     )
 }
